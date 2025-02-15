51cc871f98988fdce7465748762857a29367dc03
Abraham Bhatti <abrahambhatti@icloud.com>
2025-02-15 11:04:08 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is my first React website. Stay tuned for more updates!</p>
+    </div>
+  );
+}
+
+export default App;
